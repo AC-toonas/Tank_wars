6160c8d70f77c52aee65fa663222273e8bca5867
AC-toonas <anhong8076@gmail.com>
2025-03-14 14:32:17 -0400

    All classes
    set up all classes for basics

---
+++ Main.py
@@ -0,0 +1,63 @@
+#importantationatitationinitatimportatnte
+import pygame as p
+
+
+
+class CPU():
+    def __init__(self) -> None:
+        pass
+    def begin(self):
+        pass
+    def end(self):
+        pass
+
+
+class tank():
+    def __init__(self) -> None:
+        pass
+    def move(self):
+        pass
+    def shoot(self):
+        pass
+    def display(self):
+        pass
+
+
+class player(tank):
+    def __init__(self) -> None:
+        pass
+
+
+class enemy(tank):
+    def __init__(self) -> None:
+        pass
+
+
+class bullet():
+    def __init__(self) -> None:
+        pass
+    def move(self):
+        pass
+    def display(self):
+        pass
+
+
+class Explode():
+    def __init__(self) -> None:
+        pass
+    def explode(self):
+        pass
+
+
+class wall():
+    def __init__(self) -> None:
+        pass
+    def showwall():
+        pass
+
+
+class music():
+    def __init__(self) -> None:
+        pass
+    def play():
+        pass
